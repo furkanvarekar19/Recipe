523b913ad7dc4bacbe0938910d3ffcf31fa77d64
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-06-22 15:49:34 +0530

    update

---
+++ recipe/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign:'center'}}>
+    <h1>Recipe App</h1>
+    <input type="text" />
+    <button>serach</button>
+    </div>
+  );
+}
+
+export default App;
